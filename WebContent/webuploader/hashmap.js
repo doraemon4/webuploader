e0e536cc557af8959c8c1337ae0e7bfb3f4a3e4a
jack <xy123zk@163.com>
2018-03-31 22:07:11 +0800

    Initial commit

---
+++ WebContent/webuploader/hashmap.js
@@ -0,0 +1,34 @@
+function HashMap() {
+	this.map = {};
+}
+HashMap.prototype = {
+	put : function(key, value) {// 向Map中增加元素（key, value)
+		this.map[key] = value;
+	},
+	get : function(key) { // 获取指定Key的元素值Value，失败返回Null
+		if (this.map.hasOwnProperty(key)) {
+			return this.map[key];
+		}
+		return null;
+	},
+	remove : function(key) { // 删除指定Key的元素，成功返回True，失败返回False
+		if (this.map.hasOwnProperty(key)) {
+			return delete this.map[key];
+		}
+		return false;
+	},
+	removeAll : function() { // 清空HashMap所有元素
+		this.map = {};
+	},
+	keySet : function() { // 获取Map中所有KEY的数组（Array）
+		var _keys = [];
+		for ( var i in this.map) {
+			_keys.push(i);
+		}
+		return _keys;
+	},
+	size:function(){
+		return this.map.keySet().length;
+	}
+};
+HashMap.prototype.constructor = HashMap;
---
+++ src/com/cn/upload/Video.java
@@ -0,0 +1,117 @@
+package com.cn.upload;
+
+import java.io.File;
+import java.io.FileFilter;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.nio.channels.FileChannel;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * 合并分块
+ * @author Administrator
+ *
+ */
+@SuppressWarnings("serial")
+public class Video extends HttpServlet {
+	public void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		super.doGet(request, response);
+		doPost(request, response);
+
+	}
+
+	@SuppressWarnings("resource")
+	public void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		String savePath = this.getServletConfig().getServletContext()
+				.getRealPath("");
+		String folad = "uploads";
+		savePath = savePath + "\\" + folad + "\\";
+
+		String action = request.getParameter("action");
+
+		if (action.equals("mergeChunks")) {
+			// 合并文件
+			// 需要合并的文件的目录标记
+			String fileMd5 = request.getParameter("fileMd5");
+			String fileSuffix=request.getParameter("fileSuffix");
+
+			// 读取目录里的所有文件
+			File f = new File(savePath + "/" + fileMd5);
+			File[] fileArray = f.listFiles(new FileFilter() {
+				// 排除目录只要文件
+				public boolean accept(File pathname) {
+					if (pathname.isDirectory()) {
+						return false;
+					}
+					return true;
+				}
+			});
+
+			// 转成集合，便于排序
+			List<File> fileList = new ArrayList<File>(Arrays.asList(fileArray));
+			Collections.sort(fileList, new Comparator<File>() {
+				public int compare(File o1, File o2) {
+					if (Integer.parseInt(o1.getName()) < Integer.parseInt(o2
+							.getName())) {
+						return -1;
+					}
+					return 1;
+				}
+			});
+			// UUID.randomUUID().toString()-->随机名
+			File outputFile = new File(savePath + "/" + fileMd5 + "."+fileSuffix);
+			// 创建文件
+			outputFile.createNewFile();
+			// 输出流
+			FileChannel outChnnel = new FileOutputStream(outputFile).getChannel();
+			// 合并
+			FileChannel inChannel;
+			for (File file : fileList) {
+				inChannel = new FileInputStream(file).getChannel();
+				inChannel.transferTo(0, inChannel.size(), outChnnel);
+				inChannel.close();
+				// 删除分片
+				file.delete();
+			}
+			outChnnel.close();
+			// 清除文件夹
+			File tempFile = new File(savePath + "/" + fileMd5);
+			if (tempFile.isDirectory() && tempFile.exists()) {
+				tempFile.delete();
+			}
+			System.out.println("合并成功");
+		} else if (action.equals("checkChunk")) {
+			// 检查当前分块是否上传成功
+			String fileMd5 = request.getParameter("fileMd5");
+			String chunk = request.getParameter("chunk");
+			String chunkSize = request.getParameter("chunkSize");
+
+			File checkFile = new File(savePath + "/" + fileMd5 + "/" + chunk);
+
+			response.setContentType("text/html;charset=utf-8");
+			// 检查文件是否存在，且大小是否一致
+			if (checkFile.exists()
+					&& checkFile.length() == Integer.parseInt(chunkSize)) {
+				// 上传过
+				response.getWriter().write("{\"ifExist\":1}");
+			} else {
+				// 没有上传过
+				response.getWriter().write("{\"ifExist\":0}");
+			}
+		}
+
+	}
+
+}
